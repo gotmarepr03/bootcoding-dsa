c00092357ba3e1eef796056dc41e744e7da3b6d3
PRANAY GOTMARE <gotmarepr0@gmail.com>
2023-07-07 08:55:15 +0530

    new

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -2,25 +2,37 @@
 public class BubbleSort {
     public static void main(String[] args) {
 
-        int n = 6;
-        int A[] = { 32, 12, 23, 19, 21, 10 };
-    
+        int n = 6;/* Length of an array */
+        int A[] = { 32, 12, 23, 19, 21, 10 };/* Unsorted Array */
+
+        BubbleSort.sort(n, A);/* Calling sort Method directly by Class name becaause it is static */
+        System.out.println("Sorted Array is ");
+        BubbleSort.printArray(n, A); /* Print the Sorted Array */
+
     }
 
-    public static void sort(int n; int A[]){
+    public static void sort(int n, int A[]) {
 
-        for(int i=0;i<n;i++){
-            
-            for(int j = 0 ;j<(n-i);j++){
-
-                if( A[j]>A[j+1]){
-            /* Swapping A[j] with A[j+1] */  
-                    int temp= A[j];
-                    A[j] = A[i+j];
-                    A[j+1]=temp;
+        for (int i = 0; i < (n - 1); i++) {
+            for (int j = 0; j < (n - 1 - i); j++) {
+                /* Swapping A[j] with A[j+1] */
+                if (A[j] > A[j + 1]) {
+                    int temp = A[j];
+                    A[j] = A[j + 1];
+                    A[j + 1] = temp;
                 }
+
             }
         }
     }
-    
+
+    public static void printArray(int n, int A[]) {
+
+        for (int i = 0; i < n; i++) {
+            
+            System.out.println(A[i]);
+        }
+
+    }
+
 }
